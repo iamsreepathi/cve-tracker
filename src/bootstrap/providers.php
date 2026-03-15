1d53bcfea5309d14ae36e539eb796f32714945a9
Prashanth Sreepathi <s.prasantreddy94@gmail.com>
2026-03-15 17:18:51 -0500

    apply custom theme to filament resources

--- src/bootstrap/providers.php
+++ src/bootstrap/providers.php
@@ -1,8 +1,13 @@
 <?php
 
+use App\Providers\AppServiceProvider;
+use App\Providers\Filament\AdminPanelProvider;
+use App\Providers\Filament\AppPanelProvider;
+use App\Providers\FortifyServiceProvider;
+
 return [
-    App\Providers\AppServiceProvider::class,
-    App\Providers\Filament\AdminPanelProvider::class,
-    App\Providers\Filament\AppPanelProvider::class,
-    App\Providers\FortifyServiceProvider::class,
+    AppServiceProvider::class,
+    AdminPanelProvider::class,
+    AppPanelProvider::class,
+    FortifyServiceProvider::class,
 ];
